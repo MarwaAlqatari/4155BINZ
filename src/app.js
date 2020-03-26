fc48f7e57b4bca73eaf0ad3368ce0e0942871d8e
Radha Patel <radhapatel@Radhas-MacBook.local>
2020-03-26 16:42:42 -0400

    User SignUp

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require("express"); //importing is like making variables.
 const listingRouter = require("./controllers/ListingController");
 const connectDB = require("./db/db");
+const userRouter = require("./controllers/userController");
 
 const app = express(); //starting point for app.js
 
@@ -8,8 +9,9 @@ connectDB();
 
 app.use("/uploads", express.static("uploads"));
 app.use(express.json()); //able to read json from req
-
+app.use("/user", userRouter);
 //get post put delete crud
 app.use("/listings", listingRouter); //any req will be sent to this file
+app.use()
 
 app.listen(8081, () => console.log("Running on port 8081")); //starts server on port 8080
