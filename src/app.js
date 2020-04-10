024dca4cdf0b6ad4f5d3c892ddac65614187664c
hjamp <hjampani@uncc.edu>
2020-04-10 18:02:09 -0400

    testing

--- src/app.js
+++ src/app.js
@@ -3,12 +3,12 @@ require("dotenv").config();
 const express = require("express"); //importing is like making variables.
 const listingRouter = require("./controllers/ListingController");
 const connectDB = require("./db/db");
-HEAD
+
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
-const mongoose = require ("mongoose");
+const mongoose = require("mongoose");
 
-const userRoutes = require('./api/routes/user');
+const userRoutes = require("./routes/user");
 
 const userRouter = require("./controllers/userController");
 const cors = require("cors");
